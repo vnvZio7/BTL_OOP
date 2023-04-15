72123c600dcaad1c91de69cdcb7c690ecba15b57
lingquann <lingquin89@gmail.com>
2023-04-15 15:42:17 +0700

    commit 1

---
+++ Customer.java
@@ -0,0 +1,47 @@
+public  class Customer {
+
+    private String code;
+    private String name;
+    private String address;
+    private String phone;
+
+    public Customer(String code, String name, String address, String phone) {
+        this.code = code;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+}
